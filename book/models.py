8a74a08020dfd549e44243b504aed169e7b3c37d
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-11-18 21:27:25 +0200

    add model author

--- book/models.py
+++ book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name
