7a05f9d77ae453c63cca87d4cc1dc11293b7b45b
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-11-18 21:31:31 +0200

    create book model

--- book/models.py
+++ book/models.py
@@ -9,3 +9,13 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=10 ,decimal_places=2)
+
+    def __str__(self):
+        return self.title
+    
