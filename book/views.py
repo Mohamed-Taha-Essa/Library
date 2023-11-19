2e93c5c9fd39cc2223d2c824e358991679766a52
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-11-19 10:35:46 +0200

    add Book Author Review to admin pannel

--- book/views.py
+++ book/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
+
+
